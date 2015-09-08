86cb1cd60a7516bb465cf54259fef777de9286b8
James Barlow <jbarlow@operationalresults.com>
2015-09-07 19:58:56 -0600

    styling changes and staging stream config for production

--- app/lib/imports/user_importer.rb
+++ app/lib/imports/user_importer.rb
@@ -22,8 +22,8 @@ module Imports
       conf_token = rand(100000...999999999)
       created_at = row['created_at'] == '0000-00-00 00:00:00' ? Time.now() : row['created_at']
       updated_at = row['updated_at'] == '0000-00-00 00:00:00' ? Time.now() : row['updated_at']
-
-      sql = "insert into users (id, name, username, avatar, email, encrypted_password, confirmation_token, confirmed_at, created_at, updated_at, facebook_id, google_oauth2_id) VALUES( #{row['id']}, #{ActiveRecord::Base.connection.quote(row['name'])}, '#{row['username']}', '#{row['avatar']}', '#{row['email']}', '#{row['password']}', '#{conf_token}', '#{created_at}', '#{created_at}', '#{updated_at}', '#{row['facebook_id']}', '#{row['google_id']}' )"
+      password = row['password'].sub('$2y$10', '$2a$10')
+      sql = "insert into users (id, name, username, avatar, email, encrypted_password, confirmation_token, confirmed_at, created_at, updated_at, facebook_id, google_oauth2_id) VALUES( #{row['id']}, #{ActiveRecord::Base.connection.quote(row['name'])}, '#{row['username']}', '#{row['avatar']}', '#{row['email']}', '#{password}', '#{conf_token}', '#{created_at}', '#{created_at}', '#{updated_at}', '#{row['facebook_id']}', '#{row['google_id']}' )"
      
       ActiveRecord::Base.connection.execute(sql)
     end
--- config/initializers/stream_rails.rb
+++ config/initializers/stream_rails.rb
@@ -8,10 +8,19 @@ if Rails.env.development?
     config.location = 'us-east'
   end
 else
+  ##Production
+  # StreamRails.configure do |config|
+  #   config.api_key     = "dppcz8n6xmkc"
+  #   config.api_secret  = "a4ze5h59su875zxuthz3je7xxz4cf23g9aqtbwzs3vc7gtr2rxx6rcezqs8eapsj"
+  #   config.api_site_id = '2271'
+  #   config.location = 'us-east'
+  # end
+
+  #staging
   StreamRails.configure do |config|
-    config.api_key     = "dppcz8n6xmkc"
-    config.api_secret  = "a4ze5h59su875zxuthz3je7xxz4cf23g9aqtbwzs3vc7gtr2rxx6rcezqs8eapsj"
-    config.api_site_id = '2271'
+    config.api_key     = "bmn8pwxbswp7"
+    config.api_secret  = "jgwcam5sa96unam7726rygu5gzhcbtdfr3fuvhwhc79ra8w3pakxywz38kpjcp3s"
+    config.api_site_id = '1784'
     config.location = 'us-east'
   end
 end
